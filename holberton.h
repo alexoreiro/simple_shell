1bff8485a5397c61dafcbeaa9b89f29958d1e805
alexoreiro <alexis.oreiro@icloud.com>
2020-11-11 19:55:24 +0000

    library file

---
+++ holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+#include <stdarg.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+/**
+ * main:
+ * @abc: cmd name
+ * @def: pointer to 
+ *
+ * Description: struct for builtin functions
+ */
+
+#endif
