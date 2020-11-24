116cd6501936018f7ddb9fc504f04286ce82d9cc
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-24 11:38:47 -0300

    commit

---
+++ signal_manipulation.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * signal_manipulation - reprints prompt when signal ctrl+C is captured
+ * @signo: signal number
+ */
+void signal_manipulation(int signal)
+{
+	if (signal == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, "~$ ", 3);
+	}
+}
