fc172c4b2f1f0d1edf6564595bb496769902edfd
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-24 11:38:22 -0300

    commit

--- shell_builtin.c
+++ shell_builtin.c
@@ -30,14 +30,14 @@ return (1);
  */
 int shell_exit(char **args, char **env)
 {
-int status;
-(void)env;
+	int status;
+	(void)env;
 
-status = 0;
-if (args[1])
-status = _atoi(args[1]);
-free_array(args);
-exit(status);
+	status = 0;
+	if (args[1])
+		status = convert_to_int(args[1]);
+	free_array(args);
+	exit(status);
 }
 
 /**
@@ -48,22 +48,22 @@ exit(status);
  */
 int shell_built(char **args, char **env)
 {
-built_t b_ins[] = {
-{"env", shell_env},
-{"exit", shell_exit},
-{NULL, NULL}
-};
-int i;
+	built_t b_ins[] = {
+		{"env", shell_env},
+		{"exit", shell_exit},
+		{NULL, NULL}
+	};
+	int i;
 
-if (args[0] == NULL)
-return (0);
-for (i = 0; i < 2; i++)
-{
-if (_strcmp2(args[0], b_ins[i].cmd) == 0)
-{
-return (b_ins[i].f(args, env));
-break;
-}
-}
-return (0);
+	if (args[0] == NULL)
+		return (0);
+	for (i = 0; i < 2; i++)
+	{
+		if (_strcmp2(args[0], b_ins[i].cmd) == 0)
+		{
+			return (b_ins[i].f(args, env));
+			break;
+		}
+	}
+	return (0);
 }
