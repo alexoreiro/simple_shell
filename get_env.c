6571a03e679629cea1c11f8eade1bcbf12cfe72d
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-24 11:37:40 -0300

    commit

--- get_env.c
+++ get_env.c
@@ -8,22 +8,22 @@
  */
 char *_getenv(const char *name, char **env)
 {
-int i, j;
-int res, len;
+	int i, j;
+	int res, len;
 
-i = j = 0;
-while (env[i] != NULL)
-{
-res = _strcmp(name, (const char *)env[i]);
-if (res == 1)
-{
-len = _strlen(env[i]);
-while (env[i][j] != '=')
-j += 1;
-len -= j;
-return (env[i] + j + 1);
-}
-i++;
-}
-return (NULL);
+	i = j = 0;
+	while (env[i] != NULL)
+	{
+		res = _strcmp(name, (const char *)env[i]);
+		if (res == 1)
+		{
+			len = _strlen(env[i]);
+			while (env[i][j] != '=')
+				j += 1;
+				len -= j;
+				return (env[i] + j + 1);
+		}
+		i++;
+	}
+	return (NULL);
 }
