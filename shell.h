e82961b9c40a427fb5a268f65a0e24f53930690b
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-24 11:38:29 -0300

    commit

--- shell.h
+++ shell.h
@@ -17,8 +17,8 @@
  */
 typedef struct builtins
 {
-char *cmd;
-int (*f)(char **args, char **env);
+	char *cmd;
+	int (*f)(char **args, char **env);
 } built_t;
 
 /* Function Declarations for builtin shell commands */
@@ -35,12 +35,12 @@ int _strcmp(const char *s1, const char *s2);
 int _strcmp2(const char *s1, const char *s2);
 char **strtoken(char *str);
 void get_word(char **array, char *str);
-int word_count(char *str);
+int count_words(char *str);
 void cpy_str(int end, char *s, char **word_box);
-char *str_concat(char *s1, char *s2);
+char *concatenate_strings(char *s1, char *s2);
 void check_path(char **string, char **env);
-void signal_handler(int signo);
+void signal_manipulation(int signal);
 void e_exit(char *, int status);
 int prompt(char **ptr);
-int _atoi(char *s);
+int convert_to_int(char *s);
 #endif
